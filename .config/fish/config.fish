e9e52cd8fb6f4c9de11a6cd9f0ec80ae8314a039
Jed Fox <git@twopointzero.us>
2018-11-11 08:55:48 -0500

    Inline npx fallback

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -53,7 +53,26 @@ end
 # npx #
 #######
 # run npx by default when a command isn’t found
-source (npx --shell-auto-fallback fish | psub)
+# (result of npx --shell-auto-fallback)
+function __fish_command_not_found_on_interactive --on-event fish_prompt
+  functions --erase __fish_command_not_found_handler
+  functions --erase __fish_command_not_found_setup
+
+  function __fish_command_not_found_handler --on-event fish_command_not_found
+    if which npx > /dev/null
+        echo "$argv[1] not found. Trying with npx..." >&2
+    else
+        return 127
+    end
+    if string match -q -r @ $argv[1]
+        npx $argv
+    else
+        npx --no-install $argv
+    end
+  end
+
+  functions --erase __fish_command_not_found_on_interactive
+end
 
 ##############
 # npm Canary #
