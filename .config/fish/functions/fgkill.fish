b9744a596470c91794065faef0559a692bebd067
Jed Fox <git@jedfox.com>
2020-08-20 21:44:54 -0400

    Update fgkill

--- .config/fish/functions/fgkill.fish
+++ .config/fish/functions/fgkill.fish
@@ -1,11 +1,6 @@
-# Defined in /var/folders/r_/3vv7gfs93tg9xq8dfr83lfpm0000gn/T//fish.jURN09/fgkill.fish @ line 2
+# Defined in /var/folders/r_/3vv7gfs93tg9xq8dfr83lfpm0000gn/T//fish.RCAMI1/fgkill.fish @ line 2
 function fgkill
 	set pid (jobs --last --pid)
-	set flag $argv[1]
-	if [ -n $flag ]
-		kill -(string upper $flag) $pid
-	else
-		kill $pid
-	end
+	kill $argv $pid
 	fg $pid
 end
