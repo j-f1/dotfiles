ddab24ba8ac2862777bed34a63ea29f8cb99bfce
Jed Fox <git@jedfox.com>
2021-04-07 23:58:36 -0400

    Finicky

---
+++ .finicky-types.d.ts
@@ -0,0 +1,159 @@
+/**
+ * Finicky Configuration Reference.
+ */
+
+/**
+ * This represents the full `.finicky.js` `module.exports` object.
+ *
+ * Example:
+ *
+ * ```js
+ *  module.exports = {
+ *    defaultBrowser: "Google Chrome",
+ *    options: {
+ *      hideIcon: false
+ *    },
+ *    handlers: [{
+ *      match: finicky.matchHostnames("example.com'),
+ *      browser: "Firefox"
+ *    }]
+ *  }
+ * ```
+ */
+export interface FinickyConfig {
+  /** The default browser or app to open for urls where no other handler
+   * matches.
+   */
+  defaultBrowser: Browser | BrowserFunction | Array<Browser | BrowserFunction>;
+  options?: {
+    /** Whether or not to hide the finicky icon in the menu bar */
+    hideIcon?: boolean;
+    /** An array of domain names to replace the built in list of url
+     * shortener domains. Note that using this option replaces the list
+     * completely.
+     *
+     * Example:
+     * ```js
+     *  module.exports = {
+     *    options: {
+     *      urlShorteners: ["another-url-shortener-service.com"]
+     *    }
+     *  }
+     * ```
+     */
+    urlShorteners?: string[];
+  };
+  /** An array of Rewriters that can change the url being opened */
+  rewrite?: Rewriter[];
+  /** An array of Handlers to select which browser to open for urls */
+  handlers?: Handler[];
+}
+
+export type BrowserResult =
+  | Browser
+  | BrowserFunction
+  | Array<Browser | BrowserFunction>;
+
+/**
+ * A handler contains a matcher and a browser. If the matcher matches when opening a url, the browser in the handler will be opened.
+ */
+export interface Handler {
+  match: Matcher | Matcher[];
+  url?: PartialUrl | UrlFunction;
+  browser: BrowserResult;
+}
+
+/**
+ * A rewriter contains a matcher and a url. If the matcher matches when opening a url, the final url will be changed to whatever the url property is.
+ */
+export interface Rewriter {
+  match: Matcher | Matcher[];
+  url: PartialUrl | UrlFunction;
+}
+
+/**
+ * Matches urls (or other properties) to decide if to change the url or open a browser.
+ *
+ * If the matcher is a string, if the url equals the string exactly, it will match.
+ * If the matcher is a regular expression, if it matches any part of the url, it will match.
+ * If the matcher is a [[MatcherFunction]], it will match if the function returns `true`
+ *
+ */
+export type Matcher = string | RegExp | MatcherFunction;
+export type MatcherFunction = (options: Options) => boolean;
+
+/**
+ * Represents a browser or app that finicky could start
+ */
+export type Browser = string | BrowserObject;
+
+/**
+ * Represents a browser or app to open
+ */
+export interface BrowserObject {
+  name: string;
+  appType?: "appName" | "bundleId" | "appPath" | "none";
+  openInBackground?: boolean;
+  profile?: string;
+}
+
+/**
+ * A function that returns a browser to open
+ */
+type BrowserFunction = (options: Options) => Browser;
+
+/**
+ * Represents a url that will be handled by finicky
+ */
+export type Url = string | UrlObject;
+
+/**
+ * Represents a url that will be handled by finicky
+ */
+export type PartialUrl = string | Partial<UrlObject>;
+
+/**
+ * An object that represents a url
+ */
+export interface UrlObject {
+  protocol: string;
+  username?: string;
+  password?: string;
+  host: string;
+  port?: number;
+  pathname?: string;
+  search?: string;
+  hash?: string;
+}
+
+/**
+ * A function that returns a url
+ */
+export type UrlFunction = (options: Options) => PartialUrl;
+
+/**
+ * Options sent as the argument to [[ProcessUrl]]
+ */
+export interface ProcessOptions {
+  /** If opened in from an app, this string contains the bundle identifier from that app */
+  sourceBundleIdentifier?: string;
+  /** The state of keyboard state. E.g. shift === true if pressed. */
+  keys: {
+    shift: boolean;
+    option: boolean;
+    command: boolean;
+    control: boolean;
+    capsLock: boolean;
+    function: boolean;
+  };
+}
+
+/**
+ * Options sent as the argument to [[MatcherFunction]], [[BrowserFunction]] and [[UrlFunction]]
+ */
+export interface Options extends ProcessOptions {
+  /** The url being opened */
+  urlString: string;
+  /** The url being opened as an object */
+  url: UrlObject;
+}
---
+++ .finicky.js
@@ -0,0 +1,113 @@
+// https://github.com/johnste/finicky/wiki/Configuration
+
+const match = {
+  /**
+   * @param {string} host
+   * @returns {import("./.finicky-types").MatcherFunction}
+   */
+  host: (host) => ({ url }) => url.host === host,
+  /**
+   * @param {string} host
+   * @param {string} path
+   * @returns {import("./.finicky-types").MatcherFunction}
+   */
+  path: (host, path) => ({ url }) =>
+    url.host === host && url.pathname.startsWith(path),
+  /**
+   * @param {string} protocol
+   * @returns {import("./.finicky-types").MatcherFunction}
+   */
+  protocol: (protocol) => ({ url }) => url.protocol === protocol,
+};
+
+/**
+ * @param {string} query
+ * @param {string} key
+ */
+const getParam = (query, key) => {
+  const params = query.split("&");
+  const parsed = params.map((p) => p.split("="));
+  const match = parsed.find((p) => p[0] == key);
+  if (match) return decodeURIComponent(match[1]);
+};
+
+/**
+ * @param {string} host
+ * @param {string} protocol
+ */
+const rewriteHostToProtocol = (host, protocol) => ({
+  match: match.host(host),
+  url: ({ url }) => ({ ...url, protocol }),
+});
+
+const workOrgs = ["neevaco"];
+
+/** @type {import("./.finicky-types").FinickyConfig} */
+module.exports = {
+  defaultBrowser: "Safari Technology Preview",
+  handlers: [
+    {
+      match: ({ url }) =>
+        url.host === "github.com" &&
+        workOrgs.some((org) => url.pathname.startsWith(`/${org}/`)),
+      browser: "Google Chrome",
+    },
+    {
+      match: match.host("gather.town"),
+      // browser: "Gather",
+      browser: "Google Chrome",
+    },
+    {
+      match: match.host("apps.apple.com"),
+      browser: "App Store",
+    },
+    {
+      match: match.host("music.apple.com"),
+      browser: "Music",
+    },
+    {
+      match: match.protocol("zoommtg"),
+      browser: "us.zoom.xos",
+    },
+    {
+      match: ({ keys }) => keys.function,
+      browser: "Browserosaurus",
+    },
+    {
+      match: match.protocol("discord"),
+      browser: "Discord",
+    },
+  ],
+  rewrite: [
+    {
+      // https://l.facebook.com/l.php?u=https%253A%252F%252Ftest.jedfox.com%252F&h=[snip]&s=1
+      match: match.host("l.facebook.com"),
+      url: ({ url }) => {
+        const dest = getParam(url.search, "u");
+        if (dest) {
+          return decodeURI(decodeURIComponent(decodeURIComponent(dest)));
+        } else {
+          return url;
+        }
+      },
+    },
+    {
+      match: ({ url }) =>
+        url.host.endsWith("zoom.us") && url.pathname.startsWith("/j/"),
+      url: ({ url }) => {
+        const id = url.pathname.match(/^\/j\/([^/]+)/)?.[1];
+        return id ? `zoommtg://zoom.us/join?confno=${id}&${url.search}` : url;
+      },
+    },
+    {
+      // https://l.facebook.com/l.php?u=https%253A%252F%252Ftest.jedfox.com%252F&h=[snip]&s=1
+      match: match.host("discord.gg"),
+      url: ({ url }) => `https://discord.com/invite${url.pathname}`,
+    },
+    rewriteHostToProtocol("music.apple.com", "itms"),
+    rewriteHostToProtocol("discord.com", "discord"),
+  ],
+  options: {
+    // urlShorteners: (list) => [...list, 'l.facebook.com']
+  },
+};
