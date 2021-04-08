a2c125122b9092ba346d7a0b04efdca5d222c00d
Jed Fox <git@jedfox.com>
2021-04-07 23:48:45 -0400

    Update fish_title for vscode

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/r_/3vv7gfs93tg9xq8dfr83lfpm0000gn/T//fish.1Aap1n/fish_title.fish @ line 2
+# Defined in /var/folders/r_/3vv7gfs93tg9xq8dfr83lfpm0000gn/T//fish.8RgMN9/fish_title.fish @ line 2
 function fish_title
 	set name (basename (pwd))
 	set cmd (status current-command)
@@ -7,6 +7,14 @@ function fish_title
 			echo $cmd
 		else
 			echo ''
+		end
+	else if [ "$TERM_PROGRAM" = 'vscode' ]
+		set repo_root (git rev-parse --show-toplevel 2> /dev/null)
+		if [ $status = 0 ] && [ "$repo_root" != (pwd) ]
+			# echo (basename "$repo_root")"/"(realpath --relative-to="$repo_root" (pwd))": $cmd"
+			echo (realpath --relative-to="$repo_root" (pwd))": $cmd"
+		else
+			echo "$name: $cmd"
 		end
 	else
 		if [ "$cmd" = 'fish' ]
